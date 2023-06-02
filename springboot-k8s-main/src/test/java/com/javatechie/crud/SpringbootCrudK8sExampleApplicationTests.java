309e72c97addbd288e8cfeefcc774091fbacfb25
Pilla Karthik <130967802+pillakarthik4@users.noreply.github.com>
2023-06-02 17:58:47 +0530

    Update SpringbootCrudK8sExampleApplicationTests.java
    Change test test class added main applicatio class

--- springboot-k8s-main/src/test/java/com/javatechie/crud/SpringbootCrudK8sExampleApplicationTests.java
+++ springboot-k8s-main/src/test/java/com/javatechie/crud/SpringbootCrudK8sExampleApplicationTests.java
@@ -3,11 +3,12 @@ package com.javatechie.crud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringbootCrudK8sExampleApplication.class)
 class SpringbootCrudK8sExampleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
+
